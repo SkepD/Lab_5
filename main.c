4dc7a3b5b6eacf689d52b75c41263457c5f66630
SkepD <114313000+SkepD@users.noreply.github.com>
2023-01-10 18:02:57 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -10,10 +10,10 @@ int main()
     double x1,x2,y,delta,bin,**A;
     int number,N;
 
-    FILE* fp=fopen("file.txt", "r");
-    FILE* fp_output=fopen("file_output.txt", "w");
-    FILE* fp_binary=fopen("file_binary.txt", "w+b");
-    fscanf(fp,"%lf %lf %d %lf %s %s %s", &x1, &x2, &number, &delta, group, surname, name);
+    FILE* file=fopen("A:\\Учёба\\Предметы\\Информатика\\Лаб 5\\Lab_5\\file.txt", "r");
+    FILE* file_out=fopen("A:\\Учёба\\Предметы\\Информатика\\Лаб 5\\Lab_5\\file_output.txt", "w");
+    FILE* file_bin=fopen("A:\\Учёба\\Предметы\\Информатика\\Лаб 5\\Lab_5\\file_binary.txt", "w");
+    fscanf(file,"%lf %lf %d %lf %s %s %s", &x1, &x2, &number, &delta, group, surname, name);
 
     if (number==0)
     {
@@ -23,26 +23,26 @@ int main()
     {
         delta=(x2-x1)/(number-1);
     }
-    fclose(fp);
+    fclose(file);
 
-    fprintf(fp_output, "\t|N|X\t|Y\t\t|");
-    fwrite(&number, sizeof(int), 1, fp_binary);
+    fprintf(file_out, "\t|N|X\t|Y\t\t|");
+    fwrite(&number, sizeof(int), 1, file_bin);
     for(int i=0; i<number; i++)
 
     {
         y=calculate(x1);
-        fprintf(fp_output, "\n\t|%d|%.2lf\t|%.2lf\t|",i,x1,y);
-        fwrite(&x1, sizeof(double), 1, fp_binary);
-        fwrite(&y, sizeof(double), 1, fp_binary);
+        fprintf(file_out, "\n\t|%d|%.2lf\t|%.2lf\t|",i,x1,y);
+        fwrite(&x1, sizeof(double), 1, file_out);
+        fwrite(&y, sizeof(double), 1, file_bin);
         x1+=delta;
     }
-    fclose(fp_output);
-    fclose(fp_binary);
+    fclose(file_out);
+    fclose(file_bin);
 
-    fp_binary = fopen("file_binary.txt", "r+b");
+    file_bin = fopen("file_binary.txt", "r+b");
     printf("%s\n%s %s", group, surname,name);
     printf("\n|N|\t|X\t|\t|Y\t\t|");
-    fread(&N, sizeof(int), 1, fp_binary);
+    fread(&N, sizeof(int), 1, file_bin);
 
     A=(double**)malloc(3*sizeof(double*));
     for(int i=0; i<3; i++)
@@ -55,7 +55,7 @@ int main()
         printf("\n|%d|", i);
         for(int j=1; j<3; j++)
         {
-            fread(&bin, sizeof(double), 1, fp_binary);
+            fread(&bin, sizeof(double), 1, file_bin);
             A[j][i] = bin;
             printf("\t|%.2lf\t| ", A[j][i]);
         }
